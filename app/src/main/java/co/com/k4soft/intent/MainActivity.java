8d6afc0bdecfd350b4bc36c89ba5f45310bc8a78
diegogarcia <consultor@dgpragmatic.com>
2019-08-21 17:39:28 -0500

    Se agregan valores al intent y se hacen validaciones

--- app/src/main/java/co/com/k4soft/intent/MainActivity.java
+++ app/src/main/java/co/com/k4soft/intent/MainActivity.java
@@ -4,19 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText txtNombre;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    private void initComponents() {
+        txtNombre = findViewById(R.id.txtNombre);
     }
 
     public void goToVista2(View view) {
-        Intent intent = new Intent(getApplicationContext(),Vista2Activity.class);
-        startActivity(intent);
+        String nombre = txtNombre.getText().toString();
+        if(nombreNotEmpty(nombre)){
+            Intent intent = new Intent(getApplicationContext(),Vista2Activity.class);
+            intent.putExtra("nombre",nombre);
+            startActivity(intent);
+        }else{
+            txtNombre.setError(getString(R.string.requerido));
+        }
+
+    }
+
+    private boolean nombreNotEmpty(String nombre) {
+        return !"".equals(nombre);
     }
 }
--- app/src/main/java/co/com/k4soft/intent/Vista2Activity.java
+++ app/src/main/java/co/com/k4soft/intent/Vista2Activity.java
@@ -1,18 +1,33 @@
 package co.com.k4soft.intent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import co.com.k4soft.intent.util.ActionBarUtil;
 
 public class Vista2Activity extends AppCompatActivity {
 
+    private EditText txtNombre;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista2);
+        initComponents();
+        loadInfo();
+    }
+
+    private void loadInfo() {
+        Intent intent = getIntent();
+        txtNombre.setText(intent.getStringExtra("nombre"));
+    }
+
+    private void initComponents() {
         ActionBarUtil actionBarUtil = new ActionBarUtil(this);
         actionBarUtil.setToolBar("Activity 2","Esto es un ejemplo");
+        txtNombre = findViewById(R.id.txtNombre);
     }
 
     @Override
